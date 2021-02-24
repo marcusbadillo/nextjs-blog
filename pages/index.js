ddf8da271a30cebca05250682c7417c3bf403262
Marcus Badillo <marcusb003@hotmail.com>
2021-02-23 22:15:24 -0600

    Blog grid added.

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Blog</title>
+        <title>Next.js Blog</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
@@ -32,47 +32,135 @@ export default function Home() {
           </Link>
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <header>
+          <h1>Stuff to read?</h1>
+        </header>
+        <div class="band">
+          <div class="item-1">
+            <a href="#" class="card">
+              <div
+                class="thumb"
+                style={{
+                  backgroundImage:
+                    "url(https://images.unsplash.com/photo-1599275332750-94224515260c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=3032&q=80)",
+                }}
+              ></div>
+              <article>
+                <h1>International Artist Feature: Malaysia</h1>
+                <span>Mary Winkler</span>
+              </article>
+            </a>
+          </div>
+          <div class="item-2">
+            <a href="#" class="card">
+              <div
+                class="thumb"
+                style={{
+                  backgroundImage:
+                    "url(https://images.unsplash.com/photo-1554286923-8a066d8ec541?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1268&q=80)",
+                }}
+              ></div>
+              <article>
+                <h1>How to Roast Beef</h1>
+                <span>Harry Brignull</span>
+              </article>
+            </a>
+          </div>
+          <div class="item-3">
+            <a href="#" class="card">
+              <div
+                class="thumb"
+                style={{
+                  backgroundImage:
+                    "url(https://images.unsplash.com/photo-1547355253-ff0740f6e8c1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1164&q=80)",
+                }}
+              ></div>
+              <article>
+                <h1>Created by You, July Edition</h1>
+                <p>
+                  Welcome to our monthly feature of fantastic tutorial results
+                  created by you, the Envato Tuts+ community!{" "}
+                </p>
+                <span>Melody Nieves</span>
+              </article>
+            </a>
+          </div>
+          <div class="item-4">
+            <a href="#" class="card">
+              <div
+                class="thumb"
+                style={{
+                  backgroundImage:
+                    "url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/landing.png)",
+                }}
+              ></div>
+              <article>
+                <h1>How to Code a Scrolling “Alien Lander” Website</h1>
+                <p>
+                  We’ll be putting things together so that as you scroll down
+                  from the top of the page you’ll see an “Alien Lander” making
+                  its way to touch down.
+                </p>
+                <span>Kezz Bracey</span>
+              </article>
+            </a>
+          </div>
+          <div class="item-5">
+            <a href="#" class="card">
+              <div
+                class="thumb"
+                style={{
+                  backgroundImage:
+                    "url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/strange.jpg)",
+                }}
+              ></div>
+              <article>
+                <h1>
+                  How to Create a “Stranger Things” Text Effect in Adobe
+                  Photoshop
+                </h1>
+                <span>Rose</span>
+              </article>
+            </a>
+          </div>
+          <div class="item-6">
+            <a href="#" class="card">
+              <div
+                class="thumb"
+                style={{
+                  backgroundImage:
+                    "url(https://images.unsplash.com/photo-1512159986660-a677c02d43a7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80",
+                }}
+              ></div>
+              <article>
+                <h1>
+                  5 Inspirational Business Portraits and How to Make Your Own
+                </h1>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                <span>Marie Gardiner</span>
+              </article>
+            </a>
+          </div>
+          <div class="item-7">
+            <a href="#" class="card">
+              <div
+                class="thumb"
+                style={{
+                  backgroundImage:
+                    "url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/china.png)",
+                }}
+              ></div>
+              <article>
+                <h1>
+                  Notes From Behind the Firewall: The State of Web Design in
+                  China
+                </h1>
+                <span>Kendra Schaefer</span>
+              </article>
+            </a>
+          </div>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         main {
@@ -80,25 +168,6 @@ export default function Home() {
           flex: 1;
           display: flex;
           flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
           justify-content: center;
           align-items: center;
         }
@@ -198,7 +267,7 @@ export default function Home() {
           box-sizing: border-box;
         }
         html {
-          background-color: #fff;
+          background: #f5f7f8;
         }
         body {
           color: #111;
@@ -235,6 +304,86 @@ export default function Home() {
           flex-direction: column;
           justify-content: center;
           align-items: center;
+        }
+        header {
+          width: 90%;
+          max-width: 1240px;
+          margin: 0 auto;
+        }
+
+        .band {
+          width: 90%;
+          max-width: 1240px;
+          margin: 0 auto;
+          display: grid;
+          grid-template-columns: 1fr;
+          grid-template-rows: auto;
+          grid-gap: 20px;
+        }
+        @media (min-width: 30em) {
+          .band {
+            grid-template-columns: 1fr 1fr;
+          }
+        }
+        @media (min-width: 60em) {
+          .band {
+            grid-template-columns: repeat(4, 1fr);
+          }
+        }
+
+        .card {
+          background: white;
+          text-decoration: none;
+          color: #444;
+          box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+          display: flex;
+          flex-direction: column;
+          min-height: 100%;
+          position: relative;
+          top: 0;
+          transition: all 0.1s ease-in;
+        }
+        .card:hover {
+          top: -2px;
+          box-shadow: 0 4px 5px rgba(0, 0, 0, 0.2);
+        }
+        .card article {
+          padding: 20px;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+        }
+        .card h1 {
+          font-size: 20px;
+          margin: 0;
+          color: #333;
+        }
+        .card p {
+          flex: 1;
+          line-height: 1.4;
+        }
+        .card span {
+          font-size: 12px;
+          font-weight: bold;
+          color: #999;
+          text-transform: uppercase;
+          letter-spacing: 0.05em;
+          margin: 2em 0 0 0;
+        }
+        .card .thumb {
+          padding-bottom: 60%;
+          background-size: cover;
+          background-position: center center;
+        }
+
+        @media (min-width: 60em) {
+          .item-1 {
+            grid-column: 1 / span 2;
+          }
+          .item-1 h1 {
+            font-size: 24px;
+          }
         }
       `}</style>
     </div>
